fc0f2fbd50f2d7cf957129754172c965718f3991
Tony Morris <tmorris@tmorris.net>
2009-12-28 22:12:17 +1000

    GPX structures

--- Geo/GPX/MetadataType.hs
+++ Geo/GPX/MetadataType.hs
@@ -6,6 +6,7 @@ import Geo.GPX.LinkType
 import Geo.GPX.BoundsType
 import Geo.GPX.ExtensionsType
 import Text.XML.HXT.Arrow
+import Text.XML.HXT.Extras
 
 data MetadataType = MetadataType (Maybe String) (Maybe String) (Maybe PersonType) (Maybe CopyrightType) [LinkType] (Maybe String) (Maybe String) (Maybe BoundsType) (Maybe ExtensionsType)
   deriving (Eq, Show)
@@ -23,4 +24,13 @@ metadataType :: Maybe String
 metadataType = MetadataType
 
 instance XmlPickler MetadataType where
-  xpickle = undefined
+  xpickle = xpWrap (\(a, b, c, d, e, f, g, h, i) -> metadataType a b c d e f g h i, \(MetadataType a b c d e f g h i) -> (a, b, c, d, e, f, g, h, i)) (xp9Tuple
+              (xpOption (xpElem "name" xpText))
+              (xpOption (xpElem "desc" xpText))
+              (xpOption (xpElem "author" xpickle))
+              (xpOption (xpElem "copyright" xpickle))
+              (xpList (xpElem "link" xpickle))
+              (xpOption (xpElem "time" xpText))
+              (xpOption (xpElem "keywords" xpText))
+              (xpOption (xpElem "bounds" xpickle))
+              (xpOption (xpElem "extensions" xpickle)))
