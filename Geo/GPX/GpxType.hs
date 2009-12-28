5a9b4e1cfd2ee13d67130d5ee1e67a3699c69b2f
Tony Morris <tmorris@tmorris.net>
2009-12-28 22:20:52 +1000

    GPX structures

--- Geo/GPX/GpxType.hs
+++ Geo/GPX/GpxType.hs
@@ -6,11 +6,13 @@ import Geo.GPX.RteType
 import Geo.GPX.TrkType
 import Geo.GPX.ExtensionsType
 import Text.XML.HXT.Arrow
+import Text.XML.HXT.Extras
 
-data GpxType = GpxType String (Maybe MetadataType) [WptType] [RteType] [TrkType] (Maybe ExtensionsType)
+data GpxType = GpxType String String (Maybe MetadataType) [WptType] [RteType] [TrkType] (Maybe ExtensionsType)
   deriving (Eq, Show)
 
 gpxType :: String
+           -> String
            -> Maybe MetadataType
            -> [WptType]
            -> [RteType]
@@ -20,4 +22,12 @@ gpxType :: String
 gpxType = GpxType
 
 instance XmlPickler GpxType where
-  xpickle = undefined
+  xpickle = xpWrap (\(version, creator, metadata, wpt, rte, trk, extensions) -> gpxType version creator metadata wpt rte trk extensions,
+              \(GpxType version creator metadata wpt rte trk extensions) -> (version, creator, metadata, wpt, rte, trk, extensions)) (xp7Tuple
+                (xpAttr "version" xpText)
+                (xpAttr "creator" xpText)
+                (xpOption (xpElem "metadata" xpickle))
+                (xpList (xpElem "wpt" xpickle))
+                (xpList (xpElem "rte" xpickle))
+                (xpList (xpElem "trk" xpickle))
+                (xpOption (xpElem "extensions" xpickle)))
