d0d1a21ba4b965a9103fefadb6a1624cd5eee3c4
Tony Morris <tmorris@tmorris.net>
2009-12-28 14:22:39 +1000

    GPX structures

---
+++ Geo/GPX/Gpx.hs
@@ -0,0 +1,19 @@
+module Geo.GPX.Gpx where
+
+import Geo.GPX.Metadata
+import Geo.GPX.Wpt
+import Geo.GPX.Rte
+import Geo.GPX.Trk
+import Geo.GPX.Extensions
+
+data Gpx = Gpx (Maybe String) (Maybe Metadata) [Wpt] [Rte] [Trk] (Maybe Extensions)
+  deriving Eq
+
+gpx :: Maybe String
+    -> Maybe Metadata
+    -> [Wpt]
+    -> [Rte]
+    -> [Trk]
+    -> Maybe Extensions
+    -> Gpx
+gpx = Gpx
