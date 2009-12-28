2e46e9ad72bc842de1af9d21fc1df7f3f68612e8
Tony Morris <tmorris@tmorris.net>
2009-12-28 19:52:41 +1000

    GPX structures

--- Geo/GPX/Pt.hs
+++ Geo/GPX/Pt.hs
@@ -4,11 +4,16 @@ import Geo.GPX.Latitude
 import Geo.GPX.Longitude
 import Text.XML.HXT.Arrow
 
-data Pt = Pt Latitude Longitude [Double] [String]
+data Pt = Pt Latitude Longitude (Maybe Double) (Maybe String)
   deriving (Eq, Show)
 
-pt :: Latitude -> Longitude -> [Double] -> [String] -> Pt
+pt :: Latitude -> Longitude -> (Maybe Double) -> (Maybe String) -> Pt
 pt = Pt
 
 instance XmlPickler Pt where
-  xpickle = undefined
+  xpickle = xpWrap (\(lat, lon, ele, time) -> pt lat lon ele time,
+                   \(Pt lat lon ele time) -> (lat, lon, ele, time)) (xp4Tuple
+                     (xpAttr "lat" xpickle)
+                     (xpAttr "lon" xpickle)
+                     (xpOption (xpElem "ele" xpPrim))
+                     (xpOption (xpElem "time" xpText)))
