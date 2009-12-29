4185ed297198eb9ec628ce7287416f6d4fca4000
Tony Morris <tmorris@tmorris.net>
2009-12-29 20:24:58 +1000

    Examples

--- Data/Geo/GPX/Example/MinMaxEle.hs
+++ Data/Geo/GPX/Example/MinMaxEle.hs
@@ -6,6 +6,6 @@ import Control.Monad
 import Text.XML.HXT.Arrow
 
 -- Finds the minimum and maximum elevation (<ele>) in a list of GPX files (fails if no elevation).
-minMaxEle :: [String] -> IO (Double, Double)
+minMaxEle :: [FilePath] -> IO (Double, Double)
 minMaxEle = fmap ((minimum &&& maximum) . (maybeToList . ele =<<) . (wpts . value =<<) . join) .
                  mapM (runX . xunpickleDocument (xpickle :: PU Gpx) [(a_remove_whitespace, v_1)])
