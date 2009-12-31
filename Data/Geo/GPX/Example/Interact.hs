7c1693a951ccc0c20a234d8ae9cb30d127b77ddf
Tony Morris <tmorris@tmorris.net>
2009-12-31 21:40:22 +1000

    Interaction example and updated accessors

--- Data/Geo/GPX/Example/Interact.hs
+++ Data/Geo/GPX/Example/Interact.hs
@@ -2,6 +2,11 @@ module Data.Geo.GPX.Example.Interact where
 
 import Data.Geo.GPX
 
+-- Takes a GPX file and applies the given list of functions and writes a new GPX file.
+-- The functions are:
+-- 1) set the copyright element in the metadata with an author, year and license.
+-- 2) add a waypoint; see the home value below
+-- 3) set the creator attribute of the gpx element.
 interaction :: FilePath -> FilePath -> IO ()
 interaction = flip interactsGpx [
                 setCopyright' (copyrightType "Fred" (Just "2009") (Just "BSD3")), -- set the copyright in the metadata
