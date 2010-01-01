162d3890f25dbce492b0f00c69e2002470fe6f4c
Tony Morris <tmorris@tmorris.net>
2010-01-01 10:15:45 +1000

    Removed unused import

--- Data/Geo/GPX/Accessor/Accessor.hs
+++ Data/Geo/GPX/Accessor/Accessor.hs
@@ -2,7 +2,5 @@ module Data.Geo.GPX.Accessor.Accessor(
                                        using
                                      ) where
 
-import Control.Monad.Instances
-
 using :: (a -> d) -> (c -> a -> b) -> a -> (d -> c) -> b
 using f g = flip =<< (g .) . flip id . f
