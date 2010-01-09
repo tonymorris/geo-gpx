093dbecfbb36e368d238c017b6be01c9109c1f4f
Tony Morris <tmorris@tmorris.net>
2010-01-09 11:38:54 +1000

    comment

--- Data/Geo/GPX/Example/RemoveWaypoints.hs
+++ Data/Geo/GPX/Example/RemoveWaypoints.hs
@@ -2,6 +2,6 @@ module Data.Geo.GPX.Example where
 
 import Data.Geo.GPX
 
--- Remove all waypoints from a GPX file and writes the result to a new file.
+-- Remove all waypoints from a GPX file and write the result to a new file.
 removeWpts :: FilePath -> FilePath -> IO ()
 removeWpts = flip interactGpx (usingWpts (const []))
