560325590618b88dfc3efa9c5326cf1296a29ae9
Tony Morris <tmorris@tmorris.net>
2012-01-12 15:25:32 +1000

    show

--- src/Data/Geo/GPX/Latitude.hs
+++ src/Data/Geo/GPX/Latitude.hs
@@ -10,11 +10,15 @@ import Data.Fixed
 import Text.XML.HXT.Arrow
 
 newtype Latitude = Latitude Double
-  deriving (Eq, Ord, Show, Enum, Num, Fractional, Floating, Real, RealFrac, RealFloat)
+  deriving (Eq, Ord, Enum, Num, Fractional, Floating, Real, RealFrac, RealFloat)
 
 latitude ::
   Double -- ^ The value which will be between -90 and 90 (values out of the range are truncated using a modulus operation).
   -> Latitude
 latitude n =
   Latitude ((n + 90) `mod'` 180 - 90)
+
+instance Show Latitude where
+  show (Latitude n) = show n
+
 
