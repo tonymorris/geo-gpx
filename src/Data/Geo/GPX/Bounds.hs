131f2984f510c2bba2fb0edbc4e22abd5f81cced
Tony Morris <tmorris@tmorris.net>
2012-01-13 11:01:31 +1000

    Bounds lenses

--- src/Data/Geo/GPX/Bounds.hs
+++ src/Data/Geo/GPX/Bounds.hs
@@ -6,6 +6,12 @@ module Data.Geo.GPX.Bounds(
 
 import Data.Geo.GPX.Latitude
 import Data.Geo.GPX.Longitude
+import Data.Geo.GPX.Lens.MinlatL
+import Data.Geo.GPX.Lens.MaxlatL
+import Data.Geo.GPX.Lens.MinlonL
+import Data.Geo.GPX.Lens.MaxlonL
+import Data.Lens.Common
+import Control.Comonad.Trans.Store
 import Text.XML.HXT.Arrow
 
 data Bounds = Bounds (Latitude, Longitude) (Latitude, Longitude)
@@ -17,4 +23,20 @@ bounds ::
   -> Bounds
 bounds =
   Bounds
+
+instance MinlatL Bounds where
+  minlatL =
+    Lens $ \(Bounds (a, b) (c, d)) -> store (\a -> Bounds (a, b) (c, d)) a
+
+instance MinlonL Bounds where
+  minlonL =
+    Lens $ \(Bounds (a, b) (c, d)) -> store (\b -> Bounds (a, b) (c, d)) b
+
+instance MaxlatL Bounds where
+  maxlatL =
+    Lens $ \(Bounds (a, b) (c, d)) -> store (\c -> Bounds (a, b) (c, d)) c
+
+instance MaxlonL Bounds where
+  maxlonL =
+    Lens $ \(Bounds (a, b) (c, d)) -> store (\d -> Bounds (a, b) (c, d)) d
 
