3c0522c7c071fb573ebb86e25d66ae76e45687a2
Tony Morris <tmorris@tmorris.net>
2012-01-21 19:39:07 +1000

    I think I like it

--- src/Data/Geo/GPX.hs
+++ src/Data/Geo/GPX.hs
@@ -1,10 +1,69 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 -- | GPX 1.1 Schema <http://www.topografix.com/GPX/1/1/>
 module Data.Geo.GPX(
   module Data.Geo.GPX.Type
 , module Data.Geo.GPX.Lens
 , module Control.Arrow
+, GPX
 ) where
 
 import Data.Geo.GPX.Type
 import Data.Geo.GPX.Lens
 import Control.Arrow
+import Text.XML.HXT.Arrow.Pickle
+import Control.Newtype
+import Data.Lens.Common
+import Control.Comonad.Trans.Store
+
+-- | Top-level GPX data type with the root XML element applied.
+newtype GPX = GPX Gpx
+  deriving Eq
+
+instance XmlPickler GPX where
+  xpickle = xpWrap (GPX, \(GPX g) -> g) (xpElem "gpx" xpickle)
+
+instance GpxP GPX where
+  gpx v c m w r t e =
+    GPX (gpx v c m w r t e)
+    
+instance Newtype GPX Gpx where
+  pack = 
+    GPX
+  unpack (GPX x) =
+    x
+
+instance VersionL GPX where
+  versionL =
+    newtypeLens >>> versionL
+
+instance CreatorL GPX where
+  creatorL =
+    newtypeLens >>> creatorL
+
+instance MetadataL GPX where
+  metadataL =
+    newtypeLens >>> metadataL
+
+instance WptsL GPX where
+  wptsL =
+    newtypeLens >>> wptsL
+
+instance RtesL GPX where
+  rtesL =
+    newtypeLens >>> rtesL
+
+instance TrksL GPX where
+  trksL =
+    newtypeLens >>> trksL
+
+instance ExtensionsL GPX where
+  extensionsL =
+    newtypeLens >>> extensionsL
+
+-- not exported (belongs in data-lens)
+newtypeLens ::
+  Lens GPX Gpx
+newtypeLens =
+  Lens (store pack . unpack)
+
--- src/Data/Geo/GPX/Type/Gpx.hs
+++ src/Data/Geo/GPX/Type/Gpx.hs
@@ -3,7 +3,7 @@
 -- | Complex Type: @gpxType@ <http://www.topografix.com/GPX/1/1/#type_gpxType>
 module Data.Geo.GPX.Type.Gpx(
   Gpx
-, gpx
+, GpxP(..)
 ) where
 
 import Data.Geo.GPX.Type.Metadata
@@ -25,17 +25,20 @@ import Text.XML.HXT.Arrow.Pickle
 data Gpx = Gpx String String (Maybe Metadata) [Wpt] [Rte] [Trk] (Maybe Extensions)
   deriving Eq
 
-gpx ::
-  String -- ^ The version.
-  -> String -- ^ The creator.
-  -> Maybe Metadata -- ^ The metadata.
-  -> [Wpt] -- ^ The waypoints (wpt).
-  -> [Rte] -- ^ The routes (rte).
-  -> [Trk] -- ^ The tracks (trk).
-  -> Maybe Extensions -- ^ The extensions.
-  -> Gpx
-gpx =
-  Gpx
+class GpxP g where
+  gpx ::
+    String -- ^ The version.
+    -> String -- ^ The creator.
+    -> Maybe Metadata -- ^ The metadata.
+    -> [Wpt] -- ^ The waypoints (wpt).
+    -> [Rte] -- ^ The routes (rte).
+    -> [Trk] -- ^ The tracks (trk).
+    -> Maybe Extensions -- ^ The extensions.
+    -> g
+
+instance GpxP Gpx where
+  gpx =
+    Gpx
 
 instance VersionL Gpx where
   versionL =
@@ -68,12 +71,12 @@ instance ExtensionsL Gpx where
 instance XmlPickler Gpx where
   xpickle =
     xpWrap (\(version', creator', metadata', wpt', rte', trk', extensions') -> gpx version' creator' metadata' wpt' rte' trk' extensions',
-              \(Gpx version' creator' metadata' wpt' rte' trk' extensions') -> (version', creator', metadata', wpt', rte', trk', extensions')) (xpElem "gpx" (xp7Tuple
+              \(Gpx version' creator' metadata' wpt' rte' trk' extensions') -> (version', creator', metadata', wpt', rte', trk', extensions')) (xp7Tuple
                 (xpAttr "version" xpText)
                 (xpAttr "creator" xpText)
                 (xpOption (xpElem "metadata" xpickle))
                 (xpList (xpElem "wpt" xpickle))
                 (xpList (xpElem "rte" xpickle))
                 (xpList (xpElem "trk" xpickle))
-                (xpOption (xpElem "extensions" xpickle))))
+                (xpOption (xpElem "extensions" xpickle)))
 
