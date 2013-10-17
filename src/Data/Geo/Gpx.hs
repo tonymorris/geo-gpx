41a4d1a2fd71e45f79fad3e73f04cbabf7f5ab31
Tony Morris <tmorris@tmorris.net>
2013-10-17 18:19:38 +1000

    modules

---
+++ src/Data/Geo/Gpx.hs
@@ -0,0 +1,42 @@
+module Data.Geo.Gpx
+(
+  module Data.Geo.Gpx.Bounds
+, module Data.Geo.Gpx.Copyright
+, module Data.Geo.Gpx.Degrees
+, module Data.Geo.Gpx.DgpsStation
+, module Data.Geo.Gpx.Email
+, module Data.Geo.Gpx.Extensions
+, module Data.Geo.Gpx.Fix
+, module Data.Geo.Gpx.Gpx
+, module Data.Geo.Gpx.Latitude
+, module Data.Geo.Gpx.Link
+, module Data.Geo.Gpx.Longitude
+, module Data.Geo.Gpx.Metadata
+, module Data.Geo.Gpx.Person
+, module Data.Geo.Gpx.Pt
+, module Data.Geo.Gpx.Ptseg
+, module Data.Geo.Gpx.Rte
+, module Data.Geo.Gpx.Trk
+, module Data.Geo.Gpx.Trkseg
+, module Data.Geo.Gpx.Wpt
+) where
+
+import Data.Geo.Gpx.Bounds
+import Data.Geo.Gpx.Copyright
+import Data.Geo.Gpx.Degrees
+import Data.Geo.Gpx.DgpsStation
+import Data.Geo.Gpx.Email
+import Data.Geo.Gpx.Extensions
+import Data.Geo.Gpx.Fix
+import Data.Geo.Gpx.Gpx
+import Data.Geo.Gpx.Latitude
+import Data.Geo.Gpx.Link
+import Data.Geo.Gpx.Longitude
+import Data.Geo.Gpx.Metadata
+import Data.Geo.Gpx.Person
+import Data.Geo.Gpx.Pt
+import Data.Geo.Gpx.Ptseg
+import Data.Geo.Gpx.Rte
+import Data.Geo.Gpx.Trk
+import Data.Geo.Gpx.Trkseg
+import Data.Geo.Gpx.Wpt
--- src/Data/Geo/Gpx/Degrees.hs
+++ src/Data/Geo/Gpx/Degrees.hs
@@ -1,4 +1,8 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 -- | Simple Type: @degreesType@ <http://www.topografix.com/GPX/1/1/#type_degreesType>
 module Data.Geo.Gpx.Degrees(
@@ -8,16 +12,21 @@ module Data.Geo.Gpx.Degrees(
 ) where
 
 import Data.Fixed
+import Control.Lens
 
-newtype Degrees = Degrees Double
-  deriving (Eq, Ord, Show)
+newtype Degrees =
+ Degrees {
+   _deg :: Double
+ } deriving (Eq, Ord, Show)
 
+makeIso ''Degrees
+{-
 degrees ::
   Double -- ^ The value which will be between 0 and 360 (values out of the range are truncated using a modulus operation).
   -> Degrees
 degrees n =
   Degrees (n `mod'` 360)
-
+  -}
 runDegrees ::
   Degrees
   -> Double
