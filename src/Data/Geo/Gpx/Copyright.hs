90daef44026eb97f9b522aa118a3b3ccfa3860ac
Tony Morris <tmorris@tmorris.net>
2013-10-17 20:25:05 +1000

    copyright

--- src/Data/Geo/Gpx/Copyright.hs
+++ src/Data/Geo/Gpx/Copyright.hs
@@ -7,6 +7,7 @@
 -- | Complex Type: @copyrightType@ <http://www.topografix.com/GPX/1/1/#type_copyrightType>
 module Data.Geo.Gpx.Copyright(
   Copyright(..)
+, xpCopyrightElem
 , xpCopyright
 , author
 , year
@@ -39,35 +40,35 @@ makeFields ''Copyright
 
 -- | Pickler for @Copyright@.
 --
--- >>> fmap (unpickleDoc' xpCopyright) (runLA xread "<copyright author=\"Bob\"><year>2010</year><license>BSD3</license></copyright>")
+-- >>> fmap (unpickleDoc' xpCopyrightElem) (runLA xread "<copyright author=\"Bob\"><year>2010</year><license>BSD3</license></copyright>")
 -- [Right Copyright {author="Bob", year="2010", license="BSD3"}]
 --
--- >>> any (either (const False) (const True) . unpickleDoc' xpCopyright) (runLA xread "<copyright><year>2010</year><license>BSD3</license></copyright>")
+-- >>> any (either (const False) (const True) . unpickleDoc' xpCopyrightElem) (runLA xread "<copyright><year>2010</year><license>BSD3</license></copyright>")
 -- False
 --
--- >>> fmap (unpickleDoc' xpCopyright) (runLA xread "<copyright author=\"Bob\"><year>2010</year></copyright>")
+-- >>> fmap (unpickleDoc' xpCopyrightElem) (runLA xread "<copyright author=\"Bob\"><year>2010</year></copyright>")
 -- [Right Copyright {author="Bob", year="2010"}]
 --
--- >>> fmap (unpickleDoc' xpCopyright) (runLA xread "<copyright author=\"Bob\"><license>BSD3</license></copyright>")
+-- >>> fmap (unpickleDoc' xpCopyrightElem) (runLA xread "<copyright author=\"Bob\"><license>BSD3</license></copyright>")
 -- [Right Copyright {author="Bob", license="BSD3"}]
 --
--- >>> fmap (unpickleDoc' xpCopyright) (runLA xread "<copyright author=\"Bob\"></copyright>")
+-- >>> fmap (unpickleDoc' xpCopyrightElem) (runLA xread "<copyright author=\"Bob\"></copyright>")
 -- [Right Copyright {author="Bob"}]
 --
--- >>> fmap (unpickleDoc' xpCopyright) (runLA xread "<copyright author=\"\"><year>2010</year><license>BSD3</license></copyright>")
+-- >>> fmap (unpickleDoc' xpCopyrightElem) (runLA xread "<copyright author=\"\"><year>2010</year><license>BSD3</license></copyright>")
 -- [Right Copyright {author="", year="2010", license="BSD3"}]
 --
--- >>> fmap (unpickleDoc' xpCopyright) (runLA xread "<copyright author=\"Bob\"><year></year><license>BSD3</license></copyright>")
+-- >>> fmap (unpickleDoc' xpCopyrightElem) (runLA xread "<copyright author=\"Bob\"><year></year><license>BSD3</license></copyright>")
 -- [Right Copyright {author="Bob", year="", license="BSD3"}]
 --
--- >>> fmap (unpickleDoc' xpCopyright) (runLA xread "<copyright author=\"Bob\"><year>2010</year><license></license></copyright>")
+-- >>> fmap (unpickleDoc' xpCopyrightElem) (runLA xread "<copyright author=\"Bob\"><year>2010</year><license></license></copyright>")
 -- [Right Copyright {author="Bob", year="2010", license=""}]
 --
--- >>> any (either (const False) (const True) . unpickleDoc' xpCopyright) (runLA xread "<copyright author=\"Bob\"><x>x</x></copyright>")
+-- >>> any (either (const False) (const True) . unpickleDoc' xpCopyrightElem) (runLA xread "<copyright author=\"Bob\"><x>x</x></copyright>")
 -- False
-xpCopyright ::
+xpCopyrightElem ::
   PU Copyright
-xpCopyright =
+xpCopyrightElem =
    xpElem "copyright"
      (xpWrap
        (
@@ -78,6 +79,19 @@ xpCopyright =
          (xpAttr "author" xpText0)
          (xpOption (xpElem "year" xpText0))
          (xpOption (xpElem "license" xpText0))))
+
+xpCopyright ::
+  PU Copyright
+xpCopyright =
+   xpWrap
+     (
+       \(author', year', license') -> Copyright author' year' license'
+     , \(Copyright author' year' license') -> (author', year', license')
+     )
+     (xpTriple
+       (xpAttr "author" xpText0)
+       (xpOption (xpElem "year" xpText0))
+       (xpOption (xpElem "license" xpText0)))
 
 instance XmlPickler Copyright where
   xpickle =
