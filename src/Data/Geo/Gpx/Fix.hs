a298a9c578703d9f2eb251a474eebb66eab69db1
Tony Morris <tmorris@tmorris.net>
2013-10-21 14:51:07 +1000

    fix

--- src/Data/Geo/Gpx/Fix.hs
+++ src/Data/Geo/Gpx/Fix.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 -- | Simple Type: @fixType@ <http://www.topografix.com/GPX/1/1/#type_fixType>
 module Data.Geo.Gpx.Fix(
   Fix
@@ -8,12 +10,40 @@ module Data.Geo.Gpx.Fix(
 , dgps
 , pps
 , fix
+, xpFix
+, xpFixElem
 ) where
 
--- import Text.XML.HXT.Arrow.Pickle
+import Data.Char(toLower)
+import Text.XML.HXT.Core(XmlPickler(..), PU, xpWrapMaybe, xpText, xpElem)
+import Control.Lens(Prism', prism', (^?), (#))
+import Prelude(Eq, Ord, Show(..), Enum, Maybe(..), String, (.), map)
+-- $setup
+-- >>> import Prelude
+-- >>> import Text.XML.HXT.Core
+-- >>> let unpickleFixElem = fmap (unpickleDoc' xpFixElem) . runLA xread
+-- >>> let allUnpickledFixElem = all (either (const False) (const True) . unpickleDoc' xpFixElem) . runLA xread
 
-data Fix = None | Twod | Threed | Dgps | Pps
+data Fix =
+  None
+  | Twod
+  | Threed
+  | Dgps
+  | Pps
   deriving (Eq, Ord, Show, Enum)
+
+-- | A prism to a value which will be one of @"none"@, @"twod"@, @"threed"@, @"dgps"@, @"pps"@.
+fix ::
+  Prism' String Fix
+fix =
+  prism'
+    (map toLower . show)
+    (\s -> case s of "none" -> Just none
+                     "2d" -> Just twod
+                     "3d" -> Just threed
+                     "dgps" -> Just dgps
+                     "pps" -> Just pps
+                     _ -> Nothing)
 
 -- | The catamorphism for @Fix@.
 foldFix ::
@@ -58,19 +88,40 @@ pps ::
 pps =
   Pps
 
--- | Construct a @Fix@ using a string with a value of @["none", "2d", "3d", "dgps", "pps"]@.
-fix ::
-  String -- ^ The string value to construct a @Fix@ with.
-  -> Maybe Fix
-fix "none" = Just none
-fix "2d"= Just twod
-fix "3d" = Just threed
-fix "dgps" = Just dgps
-fix "pps" = Just pps
-fix _ = Nothing
+-- | Pickler for the @fix@ element.
+--
+-- >>> unpickleFixElem "<fix>none</fix>"
+-- [Right None]
+--
+-- >>> unpickleFixElem "<fix>2d</fix>"
+-- [Right Twod]
+--
+-- >>> unpickleFixElem "<fix>3d</fix>"
+-- [Right Threed]
+--
+-- >>> unpickleFixElem "<fix>dgps</fix>"
+-- [Right Dgps]
+--
+-- >>> unpickleFixElem "<fix>pps</fix>"
+-- [Right Pps]
+--
+-- >>> allUnpickledFixElem "<fix>abc</fix>"
+-- False
+--
+-- >>> allUnpickledFixElem "<f>none</f>"
+-- False
+xpFixElem ::
+  PU Fix
+xpFixElem =
+  xpElem "fix"
+    xpFix
 
-{-
+-- | Pickler for the @Fix@ type.
+xpFix ::
+  PU Fix
+xpFix =
+  xpWrapMaybe ((^? fix), (#) fix) xpText
+
 instance XmlPickler Fix where
   xpickle =
-    xpWrapMaybe (fix, show) xpText
--}
+    xpFix
